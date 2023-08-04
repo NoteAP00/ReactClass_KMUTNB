e72f880d3c1790da38614117598c54f9068badf2
APINUN <apinun893@gmail.com>
2023-08-04 16:27:21 +0700

    change h3

--- app1/src/test.js
+++ app1/src/test.js
@@ -1,15 +1,14 @@
 import './App.css';
 import logo from './img/windmill.png';
-
+import React from 'react';
 
 function main() {
   return (
     <div className="App">
+      <nav className="App-nav">test.js</nav>
       <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           Hello World From React
-        </p>
+        <h3>Hello World! <br/> สวัสดีชาวโลก </h3>
       </header>
       <footer className='App-footer'>
         © 2023 Apinun Umbao @KMUTNB:EnET-C
