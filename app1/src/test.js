48bd4e1b06abec359493b002a3f6b6b61a2f052c
APINUN <apinun893@gmail.com>
2023-08-04 15:30:35 +0700

    Add app1
    Test App

---
+++ app1/src/test.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+
+
+function main() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='App-logo'> Lorem </div>
+        <p>
+           Hello World From React
+        </p>
+      </header>
+      <footer className='App-footer'>
+        © 2023 Apinun Umbao @KMUTNB:EnET-C
+      </footer>
+    </div>
+  );
+}
+
+export default main;
