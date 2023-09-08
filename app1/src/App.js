eeb22cfb0e33893e865458d59651713c06969489
APINUN <apinun893@gmail.com>
2023-09-08 16:34:31 +0700

    content2 header2

--- app1/src/App.js
+++ app1/src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import React from 'react';
 
 import { userContext } from './context';
-import Header from './context-header';
-import  Content  from './context-content';
+import Header2 from './context-header2';
+import  Content2  from './context-content2';
 
 function App() {
+  let [user, setUser] = React.useState();
   return (
-      <userContext.Provider value={'Tom Jerry'}>
-        <Header/>
-        <Content/>
+      <userContext.Provider value={[user,setUser]}>
+        <Header2/>
+        <Content2/>
       </userContext.Provider>
   );
 }
