dd5a172dac25ab365a25e77189e73e32fe854bd5
APINUN <apinun893@gmail.com>
2023-09-29 15:35:07 +0700

    S11 : Bootstrap success

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,10 +1,11 @@
 // import './App.css';
 import React from 'react';
-import Router3 from './router3';
-
+// import Router3 from './router3';
+// import FormBootstrap from './form-bootstrap'
+import FormText from './form-text';
 function App() {
   return (
-      <Router3/>
+    <FormText/>
   );
 }
 
