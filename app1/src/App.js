61c72b4dca0eec299c4264964fd136004804c2da
APINUN <apinun893@gmail.com>
2023-09-29 16:45:13 +0700

    S12 : FormCheck success :100:

--- app1/src/App.js
+++ app1/src/App.js
@@ -2,10 +2,11 @@
 import React from 'react';
 // import Router3 from './router3';
 // import FormBootstrap from './form-bootstrap'
-import FormText from './form-text';
+// import FormText from './form-text';
+import FormCheck from './form-check';
 function App() {
   return (
-    <FormText/>
+    <FormCheck/>
   );
 }
 
