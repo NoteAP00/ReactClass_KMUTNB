c56a738742c38b586eb2cb710601fca33fea9efe
APINUN <apinun893@gmail.com>
2023-10-06 15:50:23 +0700

    S12:ReactHookForm_Success

--- app1/src/App.js
+++ app1/src/App.js
@@ -3,10 +3,12 @@ import React from 'react';
 // import Router3 from './router3';
 // import FormBootstrap from './form-bootstrap'
 // import FormText from './form-text';
-import FormCheck from './form-check';
+// import FormCheck from './form-check';
+// import FormSelectFile from './form-select-file';
+import FormValidation from './form-validation';
 function App() {
   return (
-    <FormCheck/>
+    <FormValidation/>
   );
 }
 
